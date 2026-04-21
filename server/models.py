6259e64ccda1b0f41c9bc6fc17d2dcc35d37f80b
Sanaya James <sanaya.j.james@gmail.com>
2026-04-20 23:09:59 -0700

    establish relationship between employee and orientation. added cascade to employee for orientation

--- server/models.py
+++ server/models.py
@@ -25,7 +25,11 @@ class Employee(db.Model):
     reviews = db.relationship('Review')
     
     # Relationship mapping the review to related reviews
-    reviews = db.relationship('Review', back_populates = "employee")
+    reviews = db.relationship('Review', back_populates = "employee", cascade='all, delete-orphan')
+
+    # Relationship mapping employee to related onboarding
+    onboarding = db.relationship(
+        'Onboarding', uselist=False, back_populates='employee', cascade='all, delete-orphan')
 
     def __repr__(self):
         return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
@@ -37,6 +41,12 @@ class Onboarding(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     orientation = db.Column(db.DateTime)
     forms_complete = db.Column(db.Boolean, default=False)
+
+    # Foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship mapping onboarding to related employee
+    employee = db.relationship('Employee', back_populates='onboarding')
 
     def __repr__(self):
         return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
--- server/seed.py
+++ server/seed.py
@@ -27,9 +27,9 @@ with app.app_context():
     db.session.commit()
 
     # 1..1 relationship between Employee and Onboarding
-    uri_onboarding = Onboarding(orientation=datetime.datetime(2023, 3, 27))
+    uri_onboarding = Onboarding(orientation=datetime.datetime(2023, 3, 27), employee=uri)
     tristan_onboarding = Onboarding(
-        orientation=datetime.datetime(2020, 1, 20, 14, 30), forms_complete=True
+        orientation=datetime.datetime(2020, 1, 20, 14, 30), forms_complete=True, employee=tristan
     )
     db.session.add_all([uri_onboarding, tristan_onboarding])
     db.session.commit()
